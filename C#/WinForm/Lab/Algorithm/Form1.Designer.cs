c3f345d38785d281e912065401089adc999c4a3d
Song2017 <BenSong2017@hotmail.com>
2017-07-20 20:31:15 +0800

    UI

--- C#/WinForm/Lab/Algorithm/Form1.Designer.cs
+++ C#/WinForm/Lab/Algorithm/Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(30, 52);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(291, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "12fds3g45,wg.r653";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Please input a string: ";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(30, 99);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(291, 20);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "12fds3g45,wg.r653";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "GetNum";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(362, 290);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- C#/WinForm/Lab/Algorithm/Form1.cs
+++ C#/WinForm/Lab/Algorithm/Form1.cs
@@ -20,11 +20,32 @@ namespace Algorithm
 
         private void Init()
         {
-            GetIntArrayFromString("12fds3g45,wg.r653");
+            //字符串取数
+            List<int> liOut;
+            this.button1.Click += delegate
+            {
+                string str = this.txtInput.Text.ToString();
+                liOut = GetIntArrayFromString("12fds3g45,wg.r653");
+
+                if (liOut != null)
+                    txtOutput.Text = GetStringFromList(liOut);
+            };
+        }
+
+        private string GetStringFromList(List<int> liOut)
+        {
+            string strOut = string.Empty;
+            if (liOut == null)
+                return strOut;
+
+            foreach (int i in liOut)
+                strOut += i.ToString() + ',';
+
+            return strOut.Length > 1 ? strOut.TrimEnd(',') : strOut;
         }
 
         //
-        private Array GetIntArrayFromString(string strSource)
+        private List<int> GetIntArrayFromString(string strSource)
         {
             if (strSource == null || strSource.Length <= 0)
                 return null;
@@ -32,24 +53,24 @@ namespace Algorithm
             char[] arySource = strSource.ToArray();
             List<int> list = new List<int>();
 
-            //取数字变数组 
             string tmp = string.Empty;
             for (int i = 0; i < arySource.Length; i++)
             {
+                //数字进行拼串
                 if (char.IsDigit(arySource[i]))
                 {
                     tmp = tmp + arySource[i].ToString();
                     if (i == arySource.Length - 1)
                         list.Add(int.Parse(tmp));
-                }
-                else if (i - 1 > 0 && char.IsDigit(arySource[i-1]))
+                }//非数字且前一位为数字添加List
+                else if (i - 1 > 0 && char.IsDigit(arySource[i - 1]))
                 {
                     list.Add(int.Parse(tmp));
                     tmp = string.Empty;
                 }
             }
-
-            return list.ToArray();
+            //list.ToArray();
+            return list;
         }
     }
 }
